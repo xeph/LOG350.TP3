f660e27973b2e46f094f176cc855bd0c190df89e
Marc-Andre Destrempes <xeph_555@hotmail.com>
2012-11-16 01:08:14 -0500

    Cleanup on the code on the main window.

--- WpfApplication2/WpfApplication2/MainWindow.xaml.cs
+++ WpfApplication2/WpfApplication2/MainWindow.xaml.cs
@@ -1,66 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfApplication2
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow
     {
+
+        private int newTabNumber;
 
         public MainWindow()
         {
             InitializeComponent();
             this.DataGrid1.ItemsSource = LoadCollectionData();
-            //this.MouseLeftButtonDown += delegate { this.DragMove(); };
             this.newTabNumber = 1;
         }
-
-        /*private void HandleAddTab(object sender, RoutedEventArgs e)
-        {
-            this.chrometabs.AddTab(this.GenerateNewItem(), false);
-        }
-
-        private void HandleAddTabAndSelect(object sender, RoutedEventArgs e)
-        {
-            this.chrometabs.AddTab(this.GenerateNewItem(), true);
-        }
-
-        private object GenerateNewItem()
-        {
-            object itemToAdd = new Button { Content = "Moo " + this.newTabNumber };
-            Interlocked.Increment(ref this.newTabNumber);
-            if (this.title.Text.Length > 0)
-            {
-                itemToAdd = new ChromeTabs.ChromeTabItem
-                {
-                    Header = this.title.Text,
-                    Content = itemToAdd
-                };
-            }
-            return itemToAdd;
-        }
-
-        private void HandleRemoveTab(object sender, RoutedEventArgs e)
-        {
-            this.chrometabs.RemoveTab(this.chrometabs.SelectedItem);
-        }*/
-
-        private int newTabNumber;
 
         private List<Author> LoadCollectionData()
         {
