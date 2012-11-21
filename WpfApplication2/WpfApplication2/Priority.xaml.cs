c6452c45ab85f27e39652a27899ccaf27540ca87
DonPellegrino <hyssar@hotmail.com>
2012-11-21 16:46:06 -0500

    priorities

--- WpfApplication2/WpfApplication2/Priority.xaml.cs
+++ WpfApplication2/WpfApplication2/Priority.xaml.cs
@@ -1,26 +1,129 @@
 ﻿using System.Windows;
+using System.Data;
 
 namespace WpfApplication2
 {
-    /// <summary>
-    /// Interaction logic for Priority.xaml
-    /// </summary>
     public partial class Priority : Window
     {
+        private Database.SQLite DB = new Database.SQLite(@"C:\Users\Don\Documents\GitHub\LOG350.TP3\WpfApplication2\WpfApplication2\ToDoAny.sqlite");
+        private DataTable loadedTable = new DataTable();
+        private System.Data.DataTable sourceDataTable;
+        private System.Data.DataTable SourceDataTable
+        {
+            get
+            {
+                return sourceDataTable;
+            }
+            set
+            {
+                sourceDataTable = value;
+                PropertiesDataGrid.ItemsSource = sourceDataTable.DefaultView;
+            }
+        }
+
         public Priority()
         {
+            this.DB.openConnection();
             WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
             InitializeComponent();
         }
 
-        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        private void Save()
         {
-            System.Console.WriteLine("SAVE BUTTON CLICKED");
+            foreach (DataRow x in SourceDataTable.Rows)
+            {
+                if (loadedTable.Select("ID = " + System.Convert.ToInt32(x["ID"])).Length == 0)
+                {
+                    //this.DB.executeQuery("insert into ");
+                }
+                else
+                {
+                    this.DB.executeQuery("update priorities set name='" + x["name"] + "', value=" + x["value"] + ", active=" + x["active"]);
+                }
+
+            }
         }
 
-        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        private void DataGrid_Initialized(object sender, System.EventArgs e)
         {
-            System.Console.WriteLine("CANCEL BUTTON CLICKED");
+            SourceDataTable = new System.Data.DataTable("Priorities");
+            SourceDataTable.Columns.Add(new System.Data.DataColumn("ID", System.Type.GetType("System.Int32")));
+            SourceDataTable.Columns.Add(new System.Data.DataColumn("Name", System.Type.GetType("System.String")));
+            SourceDataTable.Columns.Add(new System.Data.DataColumn("Value", System.Type.GetType("System.Int32")));
+            SourceDataTable.Columns.Add(new System.Data.DataColumn("Active", System.Type.GetType("System.Boolean")));
+
+            this.DB.executeQuery("SELECT * FROM 'priorities'");
+            loadedTable = DB.getTable();
+
+            foreach (DataRow x in loadedTable.Rows)
+            {
+                SourceDataTable.Rows.Add(System.Convert.ToInt32(x["ID"]), System.Convert.ToString(x["name"]), System.Convert.ToInt32(x["value"]), System.Convert.ToBoolean(x["active"]));
+            }
+
+            this.DB.closeConnection();
+        }
+
+        private void SaveButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Save();
+        }
+
+        private void SaveButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Save();
+        }
+
+        private void CloseButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void DeleteMenuItem_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            ((System.Data.DataRowView)PropertiesDataGrid.SelectedItem).Delete();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (SourceDataTable.GetChanges() != null)
+            {
+                var result = System.Windows.MessageBox.Show(this,
+                    "Voulez-vous enregistrer vos modifications?",
+                    "Modifications non-enregistrées",
+                    System.Windows.MessageBoxButton.YesNoCancel,
+                    System.Windows.MessageBoxImage.None,
+                    System.Windows.MessageBoxResult.Yes);
+
+                switch (result)
+                {
+                    case System.Windows.MessageBoxResult.Yes:
+                        Save();
+                        break;
+                    case System.Windows.MessageBoxResult.Cancel:
+                        e.Cancel = true;
+                        break;
+                }
+            }
+        }
+
+        private void Window_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            // For debugging purpose, we can show/hide the ID column with CTRL+SHIFT+F8
+            // Ok, this was not necessary for the project but was interesting to do
+
+            //if ((e.KeyboardDevice.Modifiers & ModifierKeys.Control & ModifierKeys.Shift) == (ModifierKeys.Control & ModifierKeys.Shift) && e.Key == Key.F8)
+            //{
+            //    var column = PropertiesDataGrid.Columns[0];
+            //    switch (column.Visibility)
+            //    {
+            //        case System.Windows.Visibility.Visible:
+            //            column.Visibility = System.Windows.Visibility.Hidden;
+            //            break;
+            //        case System.Windows.Visibility.Hidden:
+            //            column.Visibility = System.Windows.Visibility.Visible;
+            //            break;
+            //    }
+            //}
         }
     }
 }
---
+++ WpfApplication2/WpfApplication2/SQLite.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+
+namespace Database
+{
+    class SQLite
+    {
+        private string connectionString = string.Empty;
+        private SQLiteConnection connection;
+        private SQLiteCommand command;
+        private SQLiteDataReader reader;
+        private DataTable table;
+
+        public SQLite(String connectionString)
+        {
+            setConnectionString(connectionString);
+        }
+
+        public bool openConnection()
+        {
+            try
+            {
+                setConnection(new SQLiteConnection("Data source=" + getConnectionString() + ";Version=3;"));
+                getConnection().Open();
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                System.Diagnostics.Trace.WriteLine(System.DateTime.Now.ToString() + " :: " + e);
+                return false;
+            }
+        }
+
+        public bool closeConnection()
+        {
+            try
+            {
+                getConnection().Close();
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                System.Diagnostics.Trace.WriteLine(System.DateTime.Now.ToString() + " :: " + e);
+                return false;
+            }
+        }
+
+        public bool executeQuery(string query)
+        {
+            try
+            {
+                setTable(new DataTable());
+                setCommand(new SQLiteCommand(getConnection()));
+                this.command.CommandText = query;
+                setReader(getCommand().ExecuteReader());
+                getTable().Load(getReader());
+                getReader().Close();
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                System.Diagnostics.Trace.WriteLine(System.DateTime.Now.ToString() + " :: " + e);
+                return false;
+            }
+        }
+
+        public bool executeNonQuery(string query)
+        {
+            try
+            {
+                setCommand(new SQLiteCommand(getConnection()));
+                this.command.CommandText = query;
+                getCommand().ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                System.Diagnostics.Trace.WriteLine(System.DateTime.Now.ToString() + " :: " + e);
+                return false;
+            }
+        }
+
+        public bool beginTransaction()
+        {
+            try
+            {
+                executeNonQuery("BEGIN IMMEDIATE TRANSACTION");
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                System.Diagnostics.Trace.WriteLine(System.DateTime.Now.ToString() + " :: " + e);
+                return false;
+            }
+        }
+
+        public bool endTransaction()
+        {
+            try
+            {
+                executeNonQuery("COMMIT TRANSACTION");
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                System.Diagnostics.Trace.WriteLine(System.DateTime.Now.ToString() + " :: " + e);
+                return false;
+            }
+        }
+
+        public bool compactDatabase()
+        {
+            try
+            {
+                executeNonQuery("VACUUM");
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                System.Diagnostics.Trace.WriteLine(System.DateTime.Now.ToString() + " :: " + e);
+                return false;
+            }
+        }
+
+        private string getConnectionString()
+        {
+            return this.connectionString;
+        }
+
+        private void setConnectionString(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        private SQLiteConnection getConnection()
+        {
+            return this.connection;
+        }
+
+        private void setConnection(SQLiteConnection connection)
+        {
+            this.connection = connection;
+        }
+
+        private SQLiteCommand getCommand()
+        {
+            return this.command;
+        }
+
+        private void setCommand(SQLiteCommand command)
+        {
+            this.command = command;
+        }
+
+        private SQLiteDataReader getReader()
+        {
+            return this.reader;
+        }
+
+        private void setReader(SQLiteDataReader reader)
+        {
+            this.reader = reader;
+        }
+
+        public DataTable getTable()
+        {
+            return this.table;
+        }
+
+        public void setTable(DataTable table)
+        {
+            this.table = table;
+        }
+    }
+}
