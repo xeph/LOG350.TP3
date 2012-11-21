ef294ec28c80a8202e331c70a43ac3e7d0838fb0
authchir <martin.desharnais@gmail.com>
2012-11-21 14:07:50 -0500

    Add priorities driving table

---
+++ PartMartinToMergeWhenMainProjectCompile/PartMartinToMergeWhenMainProjectCompile/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿namespace OdbcSQLite
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : System.Windows.Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TasksListView_Initialized(object sender, System.EventArgs e)
+        {
+            var table = new System.Data.DataTable("Tasks");
+            table.Columns.Add(new System.Data.DataColumn("Name"));
+            table.Columns.Add(new System.Data.DataColumn("DeadlineID"));
+            table.Columns.Add(new System.Data.DataColumn("PriorityID"));
+            table.Columns.Add(new System.Data.DataColumn("Completion"));
+
+            var row1 = table.NewRow();
+            row1["Name"] = "Rapport du TP3";
+            row1["DeadlineID"] = 1;
+            row1["PriorityID"] = 1;
+            row1["Completion"] = 0.15;
+            table.Rows.Add(row1);
+
+            var row2 = table.NewRow();
+            row2["Name"] = "Entrevue pour un stage";
+            row2["DeadlineID"] = 2;
+            row2["PriorityID"] = 2;
+            row2["Completion"] = 0;
+            table.Rows.Add(row2);
+
+            TasksListView.ItemsSource = table.DefaultView;
+        }
+    }
+}
---
+++ PartMartinToMergeWhenMainProjectCompile/PartMartinToMergeWhenMainProjectCompile/Priorities.xaml.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections.Generic;
+using System.Windows.Input;
+
+namespace OdbcSQLite
+{
+    public partial class Priorities : System.Windows.Window
+    {
+        public Priorities()
+        {
+            InitializeComponent();
+        }
+
+        private System.Data.DataTable sourceDataTable;
+        private System.Data.DataTable SourceDataTable
+        {
+            get
+            {
+                return sourceDataTable;
+            }
+            set
+            {
+                sourceDataTable = value;
+                PropertiesDataGrid.ItemsSource = sourceDataTable.DefaultView;
+            }
+        }
+
+        private void Save()
+        {
+            // Here we should synchronize with database
+            SourceDataTable.AcceptChanges();
+        }
+
+        private void DataGrid_Initialized(object sender, System.EventArgs e)
+        {
+            // TODO: Delete all this code and replace it with a simple query when database is available.
+
+            SourceDataTable = new System.Data.DataTable("Priorities");
+            SourceDataTable.Columns.Add(new System.Data.DataColumn("ID", System.Type.GetType("System.Int32")));
+            SourceDataTable.Columns.Add(new System.Data.DataColumn("Name", System.Type.GetType("System.String")));
+            SourceDataTable.Columns.Add(new System.Data.DataColumn("Value", System.Type.GetType("System.Int32")));
+            SourceDataTable.Columns.Add(new System.Data.DataColumn("Active", System.Type.GetType("System.Boolean")));
+
+            System.Action<int, string, int, bool> AddNewRow = (id, name, value, active) =>
+                {
+                    var row = SourceDataTable.NewRow();
+                    row["ID"] = id;
+                    row["Name"] = name;
+                    row["Value"] = value;
+                    row["Active"] = active;
+                    SourceDataTable.Rows.Add(row);
+                };
+
+            AddNewRow(1, "Haute", 1, true);
+            AddNewRow(2, "Moyenne", 2, true);
+            AddNewRow(3, "Faible", 3, true);
+
+            Save();
+        }
+
+        private void SaveButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Save();
+        }
+
+        private void CloseButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void DeleteMenuItem_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            ((System.Data.DataRowView)PropertiesDataGrid.SelectedItem).Delete();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (SourceDataTable.GetChanges() != null)
+            {
+                var result = System.Windows.MessageBox.Show(this,
+                    "Voulez-vous enregistrer vos modifications?",
+                    "Modifications non-enregistrées",
+                    System.Windows.MessageBoxButton.YesNoCancel,
+                    System.Windows.MessageBoxImage.None,
+                    System.Windows.MessageBoxResult.Yes);
+
+                switch (result)
+                {
+                    case System.Windows.MessageBoxResult.Yes:
+                        Save();
+                        break;
+                    case System.Windows.MessageBoxResult.Cancel:
+                        e.Cancel = true;
+                        break;
+                }
+            }
+        }
+
+        private void Window_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            // For debugging purpose, we can show/hide the ID column with CTRL+SHIFT+F8
+            // Ok, this was not necessary for the project but was interesting to do
+
+            if ((e.KeyboardDevice.Modifiers & ModifierKeys.Control & ModifierKeys.Shift) == (ModifierKeys.Control & ModifierKeys.Shift) && e.Key == Key.F8)
+            {
+                var column = PropertiesDataGrid.Columns[0];
+                switch (column.Visibility)
+                {
+                    case System.Windows.Visibility.Visible:
+                        column.Visibility = System.Windows.Visibility.Hidden;
+                        break;
+                    case System.Windows.Visibility.Hidden:
+                        column.Visibility = System.Windows.Visibility.Visible;
+                        break;
+                }
+            }
+        }
+    }
+}
