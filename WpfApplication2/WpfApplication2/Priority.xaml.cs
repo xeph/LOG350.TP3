72ba525a35c38a33de50502cff482df9f173ed6f
authchir <martin.desharnais@gmail.com>
2012-11-21 23:06:56 -0500

    Work on priorities database mapping

--- WpfApplication2/WpfApplication2/Priority.xaml.cs
+++ WpfApplication2/WpfApplication2/Priority.xaml.cs
@@ -1,14 +1,13 @@
-﻿using System.Windows;
-using System.Data;
-using System.Collections.Generic;
+﻿using System.Windows.Input;
 
 namespace WpfApplication2
 {
-    public partial class Priority : Window
+    public partial class Priority : System.Windows.Window
     {
-        private Database.SQLite DB = new Database.SQLite(@"C:\Users\Don\Documents\GitHub\LOG350.TP3\WpfApplication2\WpfApplication2\ToDoAny.sqlite");
-        private DataTable loadedTable = new DataTable();
-        private List<System.Int32> deletedIDs = new List<System.Int32>();
+        private System.Data.SQLite.SQLiteConnection connection;
+        private System.Data.SQLite.SQLiteDataAdapter dataAdapter;
+        private System.Data.DataSet dataSet;
+
         private System.Data.DataTable sourceDataTable;
         private System.Data.DataTable SourceDataTable
         {
@@ -25,62 +24,41 @@ namespace WpfApplication2
 
         public Priority()
         {
-            this.DB.openConnection();
             WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
             InitializeComponent();
         }
 
         private void Save()
         {
-            foreach (System.Int32 x in deletedIDs)
-            {
-                this.DB.executeQuery("delete from priorities where ID=" + x);
-            }
-
-            foreach (DataRow x in SourceDataTable.Rows)
-            {
-                if (x["ID"] == System.DBNull.Value)
-                {
-                    this.DB.executeQuery("insert into priorities(name, value, active) values('"+x["name"]+"', "+x["value"]+", "+x["active"]+")");
-                }
-                else
-                {
-                    this.DB.executeQuery("update priorities set name='" + x["name"] + "', value=" + x["value"] + ", active=" + System.Convert.ToInt32(x["active"]) + " where ID=" + x["ID"]);
-                }
-
-            }
+            dataAdapter.Update(dataSet, "priorities");
         }
 
         private void DataGrid_Initialized(object sender, System.EventArgs e)
         {
-            SourceDataTable = new System.Data.DataTable("Priorities");
-            SourceDataTable.Columns.Add(new System.Data.DataColumn("ID", System.Type.GetType("System.Int32")));
-            SourceDataTable.Columns.Add(new System.Data.DataColumn("Name", System.Type.GetType("System.String")));
-            SourceDataTable.Columns.Add(new System.Data.DataColumn("Value", System.Type.GetType("System.Int32")));
-            SourceDataTable.Columns.Add(new System.Data.DataColumn("Active", System.Type.GetType("System.Boolean")));
+            connection = new System.Data.SQLite.SQLiteConnection("Data source=" + System.Environment.CurrentDirectory.ToString() + "\\ToDoAny.sqlite;Version=3;");
+            dataAdapter = new System.Data.SQLite.SQLiteDataAdapter("SELECT * FROM priorities", connection);
+            dataSet = new System.Data.DataSet();
 
-            this.DB.executeQuery("SELECT * FROM 'priorities'");
-            loadedTable = DB.getTable();
-
-            foreach (DataRow x in loadedTable.Rows)
-            {
-                SourceDataTable.Rows.Add(System.Convert.ToInt32(x["ID"]), System.Convert.ToString(x["name"]), System.Convert.ToInt32(x["value"]), System.Convert.ToBoolean(x["active"]));
-            }
-
+            dataAdapter.TableMappings.Add("priorities", "priorities");
+            var commandBuilder = new System.Data.SQLite.SQLiteCommandBuilder(dataAdapter);
             
+            dataAdapter.Fill(dataSet, "priorities");
+
+            SourceDataTable = dataSet.Tables["priorities"];
+        }
+
+        private void SaveButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Save();
         }
 
         private void CloseButton_Click(object sender, System.Windows.RoutedEventArgs e)
         {
-            this.DB.closeConnection();
             Close();
         }
 
         private void DeleteMenuItem_Click(object sender, System.Windows.RoutedEventArgs e)
         {
-            //Add ID to deletedIDs
-            object test = ((System.Data.DataRowView)PropertiesDataGrid.SelectedItem)["ID"];
-            deletedIDs.Add(1);
             ((System.Data.DataRowView)PropertiesDataGrid.SelectedItem).Delete();
         }
 
@@ -112,19 +90,19 @@ namespace WpfApplication2
             // For debugging purpose, we can show/hide the ID column with CTRL+SHIFT+F8
             // Ok, this was not necessary for the project but was interesting to do
 
-            //if ((e.KeyboardDevice.Modifiers & ModifierKeys.Control & ModifierKeys.Shift) == (ModifierKeys.Control & ModifierKeys.Shift) && e.Key == Key.F8)
-            //{
-            //    var column = PropertiesDataGrid.Columns[0];
-            //    switch (column.Visibility)
-            //    {
-            //        case System.Windows.Visibility.Visible:
-            //            column.Visibility = System.Windows.Visibility.Hidden;
-            //            break;
-            //        case System.Windows.Visibility.Hidden:
-            //            column.Visibility = System.Windows.Visibility.Visible;
-            //            break;
-            //    }
-            //}
+            if ((e.KeyboardDevice.Modifiers & ModifierKeys.Control & ModifierKeys.Shift) == (ModifierKeys.Control & ModifierKeys.Shift) && e.Key == Key.F8)
+            {
+                var column = PropertiesDataGrid.Columns[0];
+                switch (column.Visibility)
+                {
+                    case System.Windows.Visibility.Visible:
+                        column.Visibility = System.Windows.Visibility.Hidden;
+                        break;
+                    case System.Windows.Visibility.Hidden:
+                        column.Visibility = System.Windows.Visibility.Visible;
+                        break;
+                }
+            }
         }
     }
 }
